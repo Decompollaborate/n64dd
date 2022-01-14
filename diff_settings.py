44bb80f035a436c4b8e84171e248568750f22d89
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-01-14 05:12:51 +0000

    Move asm_processor, make diff.py work with folder structure add first_diff.py and sym_info.py

---
+++ diff_settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def apply(config, args):
+    config['baseimg'] = 'oot/baserom_oot_ne0_uncompressed.z64'
+    config['myimg'] = 'oot_ne0_uncompressed.z64'
+    config['mapfile'] = 'oot/ne0/build/oot_ne0.map'
+    config['makeflags'] = []
+    config['source_directories'] = ['src', 'include', 'spec']
+
