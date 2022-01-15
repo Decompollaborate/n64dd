bc8cf49f8db4ef4d61cf07e21e0f1890ee0221bb
angie <angheloalf95@gmail.com>
2022-01-15 02:55:58 -0300

    match a pair of functions

--- src/n64dd_O2_g3/n64dd_801C9B70.c
+++ src/n64dd_O2_g3/n64dd_801C9B70.c
@@ -1,12 +1,30 @@
 #include "n64dd.h"
 
+#define LANGUAGE_JP 0
+#define LANGUAGE_EN 1
+
+// i4 texture, 192*16. Error 41
+extern u8 D_801D2FE0[2][192*16/2];
+
+// I'm not completely sure about the indices
+//extern const char* D_801D2EE0[2][8][4];
+
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9B70.s")
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9C48.s")
+extern s32 gCurrentRegion;
+
+s32 func_801C9C48(void) {
+    return gCurrentRegion == 1 ? LANGUAGE_JP : LANGUAGE_EN;
+}
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9C74.s")
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9CA4.s")
+// "Error Number    " array
+extern const char* D_801D2ED0[];
+
+const char* func_801C9CA4(void) {
+    return D_801D2ED0[func_801C9C48()];
+}
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9CD4.s")
 
