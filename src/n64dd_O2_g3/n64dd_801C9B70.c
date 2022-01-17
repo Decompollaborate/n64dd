5e2559a743f7ffbd12b69a32725672281b212a47
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-01-17 01:10:57 +0000

    Match func_801C9C74

--- src/n64dd_O2_g3/n64dd_801C9B70.c
+++ src/n64dd_O2_g3/n64dd_801C9B70.c
@@ -9,7 +9,10 @@ extern u8 D_801D2FE0[2][192*16/2];
 // I'm not completely sure about the indices
 //extern const char* D_801D2EE0[2][8][4];
 
+s32 func_801C9B70(s32);
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9B70.s")
+
+
 
 extern s32 gCurrentRegion;
 
@@ -17,7 +20,12 @@ s32 func_801C9C48(void) {
     return gCurrentRegion == 1 ? LANGUAGE_JP : LANGUAGE_EN;
 }
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9C74.s")
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9C74.s")
+void func_801C9C74(u8* dest, u8 value, u32 count) {
+    while (count--) {
+        *dest++ = value;
+    }
+}
 
 // "Error Number    " array
 extern const char* D_801D2ED0[];
@@ -26,11 +34,53 @@ const char* func_801C9CA4(void) {
     return D_801D2ED0[func_801C9C48()];
 }
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9CD4.s")
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9CD4.s")
+void func_801C94F8(s8*, u16);
+s32 func_801C9B70(s32);
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9D54.s")
+// Character indices for numbers in the error code (EUC-JP)
+void func_801C9CD4(s8* arg0, s32 number) {
+    s32 temp_v0 = func_801C9B70(number);
+    u16 character;
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9DB8.s")
+    if (number >= 10) {
+        character = ((temp_v0 >> 4) + '£°'); // 0, 0xA380
+     } else { 
+         character = '¡¡'; // Space, 0xA1A1
+     }
+
+    func_801C94F8(arg0, character);
+    arg0 += 2;
+    func_801C94F8(arg0, ((temp_v0 & 0xF) + '£°')); // 0, 0xA380
+}
+
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9D54.s")
+// Character indices for numbers in the error code (ASCII)
+void func_801C9D54(s8* arg0, s32 number) {
+    s32 temp_v0 = func_801C9B70(number);
+
+    if (number >= 10) {
+        *arg0 = (temp_v0 >> 4) + '0';
+    } else {
+        *arg0 = ' ';
+    }
+    arg0++;
+    *arg0 = (temp_v0 & 0xF) + '0';
+}
+
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9DB8.s")
+void func_801C9A10(s32, UNK_TYPE, const char*);
+
+void func_801C9DB8(s32 arg0, s32 errorNum) {
+    const char* temp_v0 = func_801C9CA4();
+
+    if (gCurrentRegion == LANGUAGE_EN) {
+        func_801C9CD4(&temp_v0[12], errorNum);
+    } else {
+        func_801C9D54(&temp_v0[13], errorNum);
+    }
+    func_801C9A10(arg0, 0xC0, temp_v0);
+}
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9B70/func_801C9E28.s")
 
