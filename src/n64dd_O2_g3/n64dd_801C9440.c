21f4df009e6a23834d6555767305061e5b10f25a
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-01-17 16:28:21 +0000

    Decomp a function in 801C9440 and move to O2 g3

--- src/n64dd_O2_g3/n64dd_801C9440.c
+++ src/n64dd_O2_g3/n64dd_801C9440.c
@@ -4,7 +4,11 @@
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C9494.s")
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C94F8.s")
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C94F8.s")
+void func_801C94F8(char* arg0, u16 arg1) {
+    arg0[0] = arg1 >> 8;
+    arg0[1] = arg1 & 0xFF;
+}
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C9514.s")
 
