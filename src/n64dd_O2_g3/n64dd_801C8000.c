4a8a1668ffff755b961fbaa83c07351d5dbb207e
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-01-19 22:11:40 +0000

    Move 801C8000 to O2_g3 and decomp a couple of functions

--- src/n64dd_O2_g3/n64dd_801C8000.c
+++ src/n64dd_O2_g3/n64dd_801C8000.c
@@ -2,11 +2,26 @@
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C8000.s")
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C819C.s")
+extern s32 D_801D2E60;
+extern s32 D_801D2E64;
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C819C.s")
+void func_801C819C(UNK_PTR arg0) {
+    if (arg0 != NULL) {
+        D_801D2E60 = 1;
+    } else {
+        D_801D2E60 = 2;
+    }
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C81C4.s")
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C81C4.s")
+s32 func_801C81C4(void) {
+    return D_801D2E60;
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C81D4.s")
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C81D4.s")
+s32 func_801C81D4(void) {
+    return D_801D2E64 == 1;
+}
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C8000/func_801C81EC.s")
 
