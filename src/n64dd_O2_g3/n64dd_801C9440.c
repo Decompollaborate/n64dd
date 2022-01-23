26021636996f28b179c9f815731ca5c962434f16
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-01-23 22:03:19 +0000

    Couple more in 801C9440, start another

--- src/n64dd_O2_g3/n64dd_801C9440.c
+++ src/n64dd_O2_g3/n64dd_801C9440.c
@@ -71,12 +71,15 @@ u16 func_801C9534(u16 jisCodepoint) {
     return (hiByte << 8) + loByte;
 }
 
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C95C0.s")
+extern void (*D_801D2EC0)(s32, s32, UNK_TYPE);
+// void (*D_801D2EC0)(s32, s32, s32) = NULL;
 
-
-void func_801C95C0(s32, s32, UNK_TYPE);
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C95C0.s")
-
-// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C95F0.s")
+void func_801C95C0(s32 arg0, s32 arg1, UNK_TYPE arg2) {
+    if (D_801D2EC0 != NULL) {
+        D_801D2EC0(arg0, arg1, arg2);
+    }
+}
 
 s32 func_801C95F0(char* arg0) {
     return func_801CF0B0(func_801C9534(func_801C9514((arg0[0] << 8) | arg0[1]))) + 0xA0000;
@@ -123,15 +126,14 @@ s32 func_801C969C(UNK_TYPE arg0, UNK_PTR* arg1, UNK_PTR* arg2, UNK_PTR* arg3, u8
 UNK_TYPE4 func_801C9740(u8* arg0, UNK_TYPE4 arg1, u8 arg2) {
     if (arg1 == 1) {
         *arg0 = arg2 * 0x10;
-        return 0;
+        return false;
     } else {
         *arg0 |= arg2;
-        return 1;
+        return true;
     }
 }
 
-// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C9778.s")
-UNK_TYPE4 func_801C9778(UNK_TYPE4 arg0, UNK_TYPE4* arg1, s32 arg2) {
+s32 func_801C9778(s32 arg0, s32* arg1, s32 arg2) {
     arg0 += (arg2 >> 1);
 
     if (((*arg1 == 1) && !(arg2 & 1)) || ((*arg1 == 0) && (arg2 & 1))) {
@@ -145,7 +147,86 @@ UNK_TYPE4 func_801C9778(UNK_TYPE4 arg0, UNK_TYPE4* arg1, s32 arg2) {
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C97C4.s")
 
-// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C9954.s")
+// s32 func_801C97C4(s32 *arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, u8 *arg5, s32 arg6, s32 arg7, s32 arg8) {
+//     s32 sp4C;
+//     s32 sp44;
+//     s32 temp_lo;
+//     s32 temp_s3;
+//     s32 temp_s4;
+//     s32 temp_t2;
+//     s32 temp_v0;
+//     u8 *temp_s2;
+//     s32 phi_s6;
+//     s32 phi_s4;
+//     u8 *phi_s0;
+//     s32 phi_s1;
+//     u8 *phi_s2;
+//     s32 phi_s3;
+//     s32 phi_s7;
+//     u8 *phi_s2_2;
+//     s32 phi_s4_2;
+//     u8 *phi_fp;
+//     u8 *phi_s2_3;
+//     u8 *phi_s2_4;
+
+//     sp44 = arg6 + arg3 + arg4;
+//     sp4C = arg2;
+//     temp_v0 = func_801C9778(*arg0, &sp4C, arg3);
+//     temp_t2 = arg1 >> 1;
+//     temp_lo = (0xB - arg8) * temp_t2;
+//     arg1 = temp_t2;
+//     phi_s6 = arg6;
+//     phi_s4_2 = 1;
+//     phi_fp = temp_v0 + temp_lo;
+//     phi_s2_3 = arg5;
+//     if ((arg6 & 1) != 0) {
+//         phi_s6 = arg6 + 1;
+//     }
+//     phi_s7 = arg7 - 1;
+//     if (arg7 != 0) {
+//         do {
+//             phi_s4 = phi_s4_2;
+//             phi_s0 = phi_fp;
+//             phi_s1 = sp4C;
+//             phi_s2 = phi_s2_3;
+//             phi_s3 = 0;
+//             phi_s2_2 = phi_s2_3;
+//             if (phi_s6 > 0) {
+//                 do {
+//                     phi_s2_4 = phi_s2;
+//                     phi_s2_4 = phi_s2;
+//                     if (phi_s4 == 1) {
+//                         if (func_801C9740(phi_s0, phi_s1, (*phi_s2 >> 4))) {
+//                             phi_s0++;
+//                         }
+//                     } else {
+//                         temp_s2 = phi_s2 + 1;
+//                         phi_s2_4 = temp_s2;
+//                         phi_s2_4 = temp_s2;
+//                         if (func_801C9740(phi_s0, phi_s1, *phi_s2 & 0xF)) {
+//                             phi_s0++;
+//                         }
+//                     }
+//                     temp_s3 = phi_s3 + 1;
+//                     temp_s4 = phi_s4 ^ 1;
+//                     phi_s4 = temp_s4;
+//                     phi_s1 ^= 1;
+//                     phi_s2 = phi_s2_4;
+//                     phi_s3 = temp_s3;
+//                     phi_s2_2 = phi_s2_4;
+//                     phi_s4_2 = temp_s4;
+//                 } while (temp_s3 != phi_s6);
+//             }
+//             phi_s7 += -1;
+//             phi_fp += arg1;
+//             phi_s2_3 = phi_s2_2;
+//         } while (phi_s7 != 0);
+//         arg5 = phi_s2_2;
+//     }
+//     *arg0 = func_801C9778(*arg0, &arg2, sp44);
+//     return arg2;
+// }
+
 void func_801C9954(u8* bytes, s32* arg1, s32* arg2) {
     u8 prevCh;
     u8 nextCh;
@@ -193,4 +274,8 @@ void func_801C9954(u8* bytes, s32* arg1, s32* arg2) {
 void func_801C9A10(u8*, s32, char*);
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C9A10.s")
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C9B50.s")
+// #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801C9440/func_801C9B50.s")
+void func_801C9B50(s32 arg0, void (*arg1)(s32, s32, s32)) {
+    B_801E0F70 = arg0;
+    D_801D2EC0 = arg1;
+}
