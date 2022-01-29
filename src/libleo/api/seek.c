61caaa1483cb82f95143658ba43119f5ce5c4358
angie <angheloalf95@gmail.com>
2022-01-29 00:40:24 -0300

    match seek.c

--- src/libleo/api/seek.c
+++ src/libleo/api/seek.c
@@ -1,4 +1,23 @@
 #include "n64dd.h"
 #include "n64dd_functions.h"
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/seek/LeoSeek.s")
+s32 LeoSeek(LEOCmd* cmdBlock, u32 lba, OSMesgQueue* mq) {
+    if (!__leoActive) {
+        return -1;
+    }
+
+    cmdBlock->header.command = LEO_COMMAND_READ_DISK_ID;
+    cmdBlock->header.reserve1 = 0;
+    cmdBlock->header.control = 0;
+    cmdBlock->header.reserve3 = 0;
+    cmdBlock->data.readWrite.lba = lba;
+
+    if (mq != NULL) {
+        cmdBlock->header.control |= LEO_CONTROL_POST;
+        cmdBlock->header.post = mq;
+    }
+
+    leoCommand(cmdBlock);
+
+    return 0;
+}
