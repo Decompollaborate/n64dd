ea739fbd6775941943958ad1cebed4ee03823fec
angie <angheloalf95@gmail.com>
2022-01-31 16:56:38 -0300

    testunitready.c

---
+++ src/libleo/api/testunitready.c
@@ -0,0 +1,11 @@
+#include "n64dd.h"
+#include "n64dd_functions.h"
+
+#if 0
+s32 LeoTestUnitReady(LEOStatus* status);
+//{
+//    volatile LEOCmdTestUnitReady cmdBlock;
+//}
+#endif
+
+#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/testunitready/LeoTestUnitReady.s")
