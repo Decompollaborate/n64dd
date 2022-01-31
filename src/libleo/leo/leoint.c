6d52f048e10268b0800f5c48a982971fa79de1c0
angie <angheloalf95@gmail.com>
2022-01-31 14:17:03 -0300

    add mdebug stuff to leoint

--- src/libleo/leo/leoint.c
+++ src/libleo/leo/leoint.c
@@ -1,6 +1,52 @@
 #include "n64dd.h"
 #include "n64dd_functions.h"
 
+#if 0
+void leointerrupt(void* arg);
+//{
+//    u32 result;
+//    u32 tg_blocks;
+//    Label: complete @ 364;
+//}
+
+// static
+u32 read_write_track();
+//{
+//    u32 message;
+//    u32 block;
+//    u32 retry_cntr;
+//    block_param_form block_param;
+//    {
+//        u16 bnum;
+//    }
+//    {
+//        u8* temp;
+//        u32 c2datasize;
+//    }
+//    Label: track_end @ 2248;
+//    Label: do_retry @ 2276;
+//}
+
+// static
+u32 leoChk_mecha_int();
+//{
+//    u32 stat;
+//    u32 index_stat;
+//}
+
+// static
+void leosetup_BM();
+//{
+//}
+
+// static
+u32 leochk_err_reg();
+//{
+//    u32 sense;
+//    u32 index_status;
+//}
+#endif
+
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leoint/leointerrupt.s")
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leoint/func_801CADD0.s")
