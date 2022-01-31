891d46fa44ccda693c7b6e3bb6465d28b2a76ea0
angie <angheloalf95@gmail.com>
2022-01-31 14:38:00 -0300

    add mdebug stuff to leomseq_tbl

--- src/libleo/leo/leomseq_tbl.c
+++ src/libleo/leo/leomseq_tbl.c
@@ -1,4 +1,20 @@
 #include "n64dd.h"
 #include "n64dd_functions.h"
 
+#if 0
+static u32 mseq_tbl[16];
+
+static const u32 rd_mseq_code[16];
+
+static const u32 wt_mseq_code[16];
+
+void leoSet_mseq(u16 rwmode);
+//{
+//    u32* tbl;
+//    u32 sct_byte_x;
+//    u32 sct_byte_u;
+//    u8 i;
+//}
+#endif
+
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leomseq_tbl/leoSet_mseq.s")
