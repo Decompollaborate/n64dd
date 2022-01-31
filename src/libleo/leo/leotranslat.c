cad2e129799cc2a58b9444fdc1b1648898fe582a
angie <angheloalf95@gmail.com>
2022-01-31 18:20:45 -0300

    leotranslat

---
+++ src/libleo/leo/leotranslat.c
@@ -0,0 +1,17 @@
+#include "n64dd.h"
+#include "n64dd_functions.h"
+
+#if 0
+void leoTranslate();
+//{
+//    u32 lba;
+//    u32 calc_bytes;
+//    u32 calc_blks;
+//    u32 byte_p_blk;
+//    u16 zone;
+//    u16 vzone;
+//    u8 flag;
+//}
+#endif
+
+#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leotranslat/leoTranslate.s")
