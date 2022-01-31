0837456d29223ae0c728c18aba87db0e8c054223
angie <angheloalf95@gmail.com>
2022-01-31 14:15:10 -0300

    leoint

---
+++ src/libleo/leo/leoint.c
@@ -0,0 +1,12 @@
+#include "n64dd.h"
+#include "n64dd_functions.h"
+
+#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leoint/leointerrupt.s")
+
+#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leoint/func_801CADD0.s")
+
+#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leoint/func_801CB3CC.s")
+
+#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leoint/func_801CB434.s")
+
+#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leoint/func_801CB51C.s")
