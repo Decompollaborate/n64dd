06d68daaabf1a6bbd0edaec96557bdcf72fb5995
angie <angheloalf95@gmail.com>
2022-01-31 17:29:06 -0300

    add mdebug stuff to spdlmotor

--- src/libleo/api/spdlmotor.c
+++ src/libleo/api/spdlmotor.c
@@ -1,4 +1,10 @@
 #include "n64dd.h"
 #include "n64dd_functions.h"
 
+#if 0
+s32 LeoSpdlMotor(LEOCmd* cmdBlock, LEOSpdlMode mode, OSMesgQueue* mq);
+//{
+//}
+#endif
+
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/spdlmotor/LeoSpdlMotor.s")
