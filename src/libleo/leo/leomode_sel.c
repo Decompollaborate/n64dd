721c9a26874d54c98f68b3e26916d5828bb20e51
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-02-02 01:16:24 +0000

    Improve goto in leomode_sel, maybe?

--- src/libleo/leo/leomode_sel.c
+++ src/libleo/leo/leomode_sel.c
@@ -14,15 +14,12 @@ void leoMode_sel(void) {
     }
     sense = leoSend_asic_cmd_w_nochkDiskChange(0x150000, LEOcur_command->data.readWrite.transferBlks);
     if (sense != LEO_SENSE_NO_ADDITIONAL_SENSE_INFOMATION) {
-        goto mselerror;
-    }
-
-    if (0) {
     mselerror:
         LEOcur_command->header.sense = sense;
         LEOcur_command->header.status = LEO_STATUS_CHECK_CONDITION;
-    } else {
-        LEOcur_command->header.sense = sense;
-        LEOcur_command->header.status = LEO_STATUS_GOOD;
+        return;
     }
+
+    LEOcur_command->header.sense = sense;
+    LEOcur_command->header.status = LEO_STATUS_GOOD;
 }
