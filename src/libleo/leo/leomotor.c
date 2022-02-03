a93ce5433f89cc0b3a924bc274c8c74cfdc93f16
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-02-03 21:01:21 +0000

    leomotor OK (thanks Tharo)

--- src/libleo/leo/leomotor.c
+++ src/libleo/leo/leomotor.c
@@ -2,14 +2,35 @@
 #include "n64dd_functions.h"
 #include "libleo_functions.h"
 
-#if 0
-void leoStart_stop();
-//{
-//    u32 send_cmd;
-//    u8 sense_code;
-//    u8 retry_cntr;
-//    u32 send_data;
-//}
-#endif
+void leoStart_stop(void) {
+    u32 send_cmd;
+    u8 sense_code;
+    u8 retry_cntr = 8;
+    u32 send_data;
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leomotor/leoStart_stop.s")
+    do {
+        send_data = 0;
+        if ((LEOcur_command->header.control & 1)) {
+            send_cmd = 0x50001;
+        } else if ((LEOcur_command->header.control & 2)) {
+            send_cmd = 0xD0000;
+        } else {
+            if ((LEOcur_command->header.control & 4)) {
+                send_data = 0x10000;
+            }
+            send_cmd = 0x40000;
+        }
+        sense_code = leoSend_asic_cmd_w_nochkDiskChange(send_cmd, send_data);
+        if (sense_code == 0) {
+            LEOcur_command->header.status = 0;
+            return;
+        }
+        if (leoChk_err_retry(sense_code)) {
+            break;
+        }
+    } while (retry_cntr--);
+
+    LEOcur_command->header.sense = sense_code;
+    LEOcur_command->header.status = 2;
+    return;
+}
