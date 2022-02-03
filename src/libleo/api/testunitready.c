94d3beec648041773a406a12d8cb137e7ea55c67
angie <angheloalf95@gmail.com>
2022-02-03 18:52:29 -0300

    LeoTestUnitReady

--- src/libleo/api/testunitready.c
+++ src/libleo/api/testunitready.c
@@ -2,11 +2,27 @@
 #include "n64dd_functions.h"
 #include "libleo_functions.h"
 
-#if 0
-s32 LeoTestUnitReady(LEOStatus* status);
-//{
-//    volatile LEOCmdTestUnitReady cmdBlock;
-//}
-#endif
+s32 LeoTestUnitReady(u8* status) {
+    volatile LEOCmdTestUnitReady cmdBlock;
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/testunitready/LeoTestUnitReady.s")
+    if (!__leoActive) {
+        return -1;
+    }
+
+    if ((HW_REG(PI_STATUS_REG, u32) & 1) != 0) {
+        return 8;
+    }
+
+    cmdBlock.header.command = 3;
+    cmdBlock.header.reserve1 = 0;
+    cmdBlock.header.control = 0;
+    cmdBlock.header.reserve3 = 0;
+
+    leoCommand(&cmdBlock);
+
+    while (cmdBlock.header.status == 8) {
+    }
+
+    *status = cmdBlock.test;
+    return cmdBlock.header.sense;
+}
