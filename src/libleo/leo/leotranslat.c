5b67023795d2d6f65e1fd8614832c8944a33487e
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-02-03 23:41:48 +0000

    leotranslat OK

--- src/libleo/leo/leotranslat.c
+++ src/libleo/leo/leotranslat.c
@@ -2,17 +2,81 @@
 #include "n64dd_functions.h"
 #include "libleo_functions.h"
 
-#if 0
-void leoTranslate();
-//{
-//    u32 lba;
-//    u32 calc_bytes;
-//    u32 calc_blks;
-//    u32 byte_p_blk;
-//    u16 zone;
-//    u16 vzone;
-//    u8 flag;
-//}
-#endif
+void leoTranslate(void) {
+    u32 lba;
+    u32 calc_bytes;
+    u32 calc_blks;
+    u32 byte_p_blk;
+    u16 zone;
+    u16 vzone;
+    u8 flag; // boolean
+    if (LEOcur_command->data.readWrite.lba >= NUM_LBAS) {
+        LEOcur_command->header.sense = LEO_SENSE_LBA_OUT_OF_RANGE;
+        LEOcur_command->header.status = LEO_STATUS_CHECK_CONDITION;
+        return;
+    }
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leotranslat/leoTranslate.s")
+    lba = LEOcur_command->data.readWrite.lba + 0x18;
+    calc_blks = 0;
+    calc_bytes = 0;
+    flag = vzone = 1;
+
+    if (LEOcur_command->header.control & LEO_CONTROL_TBL) {
+        calc_bytes = LEOcur_command->data.readWrite.transferBlks;
+        while (calc_bytes != 0) {
+            if (flag || (LEOVZONE_TBL[LEOdisk_type][vzone] == lba)) {
+                vzone = leoLba_to_vzone(lba);
+                zone = LEOVZONE_PZONEHD_TBL[LEOdisk_type][vzone];
+                if (zone >= 8) {
+                    zone -= 7;
+                }
+                byte_p_blk = LEOBYTE_TBL2[zone];
+            }
+
+            // calc_bytes = ((calc_bytes < byte_p_blk) ? 0 : calc_bytes - byte_p_blk);
+            if (calc_bytes < byte_p_blk) {
+                calc_bytes = 0;
+            } else {
+                calc_bytes -= byte_p_blk;
+            }
+
+            calc_blks++;
+            lba++;
+            if ((calc_bytes != 0) && (lba >= 0x10DC)) {
+                LEOcur_command->header.sense = LEO_SENSE_LBA_OUT_OF_RANGE;
+                LEOcur_command->header.status = LEO_STATUS_CHECK_CONDITION;
+                return;
+            }
+            flag = false;
+        }
+
+        LEOcur_command->data.readWrite.buffPtr = calc_blks;
+    } else {
+        calc_blks = LEOcur_command->data.readWrite.transferBlks;
+        while (calc_blks != 0) {
+            if (flag || (LEOVZONE_TBL[LEOdisk_type][vzone] == lba)) {
+                vzone = leoLba_to_vzone(lba);
+                zone = LEOVZONE_PZONEHD_TBL[LEOdisk_type][vzone];
+                if (zone >= 8) {
+                    zone -= 7;
+                }
+                byte_p_blk = LEOBYTE_TBL2[zone];
+            }
+
+            calc_bytes += byte_p_blk;
+
+            calc_blks--;
+            lba++;
+            if ((calc_blks != 0) && (lba >= 0x10DC)) {
+                LEOcur_command->header.sense = LEO_SENSE_LBA_OUT_OF_RANGE;
+                LEOcur_command->header.status = LEO_STATUS_CHECK_CONDITION;
+                return;
+            }
+            flag = false;
+        }
+
+        LEOcur_command->data.readWrite.buffPtr = calc_bytes;
+    }
+
+    LEOcur_command->header.status = LEO_STATUS_GOOD;
+}
