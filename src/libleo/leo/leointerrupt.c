d1643fe5e9b53129b8bb555cc4b0e4db19ca0caa
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-02-04 04:54:17 +0000

    Match bottom 2 functions in leointerrupt

--- src/libleo/leo/leointerrupt.c
+++ src/libleo/leo/leointerrupt.c
@@ -1,6 +1,12 @@
 #include "n64dd.h"
 #include "n64dd_functions.h"
 #include "libleo_functions.h"
+#include "ultra64/internal.h"
+
+extern OSThread* __osRunQueue;
+extern OSHWIntr __OSGlobalIntMask;
+void __osLeoAbnormalResume(void);
+void __osLeoResume(void);
 
 #if 0
 s32 __osLeoInterrupt();
@@ -36,10 +42,45 @@ void __osLeoResume();
 
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leointerrupt/__osLeoInterrupt.s")
 
-// static
-void __osLeoAbnormalResume();
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leointerrupt/__osLeoAbnormalResume.s")
 
 // static
-void __osLeoResume();
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/leointerrupt/__osLeoResume.s")
+void __osLeoAbnormalResume(void) {
+    __OSTranxInfo* info;
+    u32 pi_stat;
+
+    info = &__osDiskHandle->transferInfo;
+    pi_stat = HW_REG(PI_STATUS_REG, u32);
+    while (pi_stat & 3) {
+        pi_stat = HW_REG(PI_STATUS_REG, u32);
+    }
+
+    HW_REG(0x5000510, u32) = info->bmCtlShadow | 0x10000000;
+    pi_stat = HW_REG(PI_STATUS_REG, u32);
+    while (pi_stat & 3) {
+        pi_stat = HW_REG(PI_STATUS_REG, u32);
+    }
+
+    HW_REG(0x5000510, u32) = info->bmCtlShadow;
+    __osLeoResume();
+    HW_REG(PI_STATUS_REG, u32) = 2;
+    __OSGlobalIntMask |= 0x100401;
+}
+
+// static
+void __osLeoResume(void) {
+    __OSEventState* es;
+    OSMesgQueue* mq;
+    s32 last;
+
+    es = &__osEventStateTab[8];
+    mq = es->queue;
+    if ((mq == 0) || (mq->validCount >= mq->msgCount)) {
+        return;
+    }
+    last = (mq->first + mq->validCount) % mq->msgCount;
+    mq->msg[last] = es->msg;
+    mq->validCount++;
+    if (mq->mtqueue->next != NULL) {
+        __osEnqueueThread(&__osRunQueue, __osPopThread(&mq->mtqueue));
+    }
+}
