8cb9173f24170e8b66d322007d464b710787a92d
cadmic <cadmic24@gmail.com>
2024-08-04 23:32:21 -0700

    Make some progress on gfxprint-like thing

--- include/code_functions.h
+++ include/code_functions.h
@@ -3,6 +3,8 @@
 
 #include "ultra64.h"
 
+s32 PrintUtils_VPrintf(PrintCallback* pfn, const char* fmt, va_list args);
+
 void Fault_AddHungupAndCrash(const char* filename, u32 line);
 
 void func_800AD51C(void);
--- src/n64dd_O2_g3/n64dd_801CA0B0.c
+++ src/n64dd_O2_g3/n64dd_801CA0B0.c
@@ -2,6 +2,33 @@
 #include "n64dd_functions.h"
 #include "libleo_functions.h"
 
+// from color.h
+typedef union Color_RGBA8_u32 {
+    struct {
+        u8 r, g, b, a;
+    };
+    u32 rgba;
+} Color_RGBA8_u32;
+
+// Similar to GfxPrint
+typedef struct struct_801CA704 {
+    /* 0x00 */ PrintCallback callback;
+    /* 0x04 */ s32 unk4;
+    /* 0x08 */ u16 unk8;
+    /* 0x0A */ u16 posX;
+    /* 0x0C */ u16 posY;
+    /* 0x0E */ u16 unkE;
+    /* 0x10 */ Color_RGBA8_u32 color;
+    /* 0x14 */ u16 baseX;
+    /* 0x16 */ u16 baseY;
+    /* 0x18 */ u16 unk18;
+    /* 0x1A */ u16 unk1A;
+    /* 0x1C */ u8 unk1C;
+    /* 0x20 */ s32 unk20;
+    /* 0x24 */ u16 unk24;
+    /* 0x26 */ u16 unk26;
+} struct_801CA704;
+
 #ifdef NON_MATCHING
 // TODO: size and values?
 u32 D_801D8B60[1];
@@ -42,6 +69,7 @@ s32 func_801CA0B0(s32 arg0, s32 arg1, s32* dx, s32* dy, s32* cy) {
     return 0;
 }
 #else
+s32 func_801CA0B0(s32 arg0, s32 arg1, s32* dx, s32* dy, s32* cy);
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA0B0.s")
 #endif
 
@@ -99,33 +127,159 @@ void func_801CA1F0(u8* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s
     }
 }
 #else
+void func_801CA1F0(u8* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s16* arg6, s32 arg7);
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA1F0.s")
 #endif
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA2F8.s")
+void func_801CA2F8(struct_801CA704* arg0, u32 r, u32 g, u32 b, u32 a) {
+    arg0->color.r = r;
+    arg0->color.g = g;
+    arg0->color.b = b;
+    arg0->color.a = a;
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA314.s")
+void func_801CA314(struct_801CA704* arg0, s32 arg1, s32 arg2) {
+    arg0->posX = arg0->baseX + arg1;
+    arg0->posY = arg0->baseY + arg2;
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA334.s")
+void func_801CA334(struct_801CA704* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4) {
+    arg0->baseX = arg1;
+    arg0->baseY = arg2;
+    arg0->unk18 = arg3;
+    arg0->unk1A = arg4;
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA350.s")
+void func_801CA350(struct_801CA704* arg0, s32 arg1, s32 arg2, s32 arg3) {
+    arg0->unk20 = arg1 + 0x20000000;
+    arg0->unk24 = arg2;
+    arg0->unk26 = arg3;
+    func_801CA334(arg0, 0, 0, arg2 - 1, arg3 - 1);
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA3B4.s")
+void func_801CA3B4(struct_801CA704* arg0, s32 arg1, s32 arg2) {
+    arg0->unk4 = arg1 + 0x20000000;
+    arg0->unk8 = arg2;
+}
 
+#if NON_MATCHING
+void func_801CA3CC(struct_801CA704* arg0, char c) {
+    s32 temp_v0_2;
+    s32 var_a0;
+    s32 var_v0;
+    s32 sp48;
+    s32 sp44;
+    s32 sp40;
+    u16 temp_t1;
+    u16 var_a1;
+    u8 temp_v0;
+
+    temp_v0 = arg0->unk1C;
+    if (temp_v0 != 0) {
+        var_a0 = (temp_v0 << 8) | c;
+    } else {
+        if (c >= 0x80 && c < 0x99) {
+            arg0->unk1C = c;
+            return;
+        }
+        var_a0 = c;
+    }
+
+    arg0->unk1C = 0;
+    if (func_801CA0B0(var_a0, arg0->unk4, &sp48, &sp44, &sp40) == 0) {
+        var_a1 = arg0->posX;
+        if (arg0->unk18 < var_a1 + sp48) {
+            temp_t1 = arg0->baseX;
+            temp_v0_2 = arg0->posY + 0x10;
+            arg0->posX = temp_t1;
+            if (arg0->unk1A < temp_v0_2) {
+                var_a1 = temp_t1 & 0xFFFF;
+                arg0->posY = arg0->baseY;
+            } else {
+                arg0->posY = temp_v0_2;
+                var_a1 = arg0->posX;
+            }
+        }
+        func_801CA1F0(arg0->unk4, var_a1, arg0->posY, sp48, sp44, sp40, arg0->unk20, arg0->unk24);
+        var_v0 = sp48 == 0x10 ? sp48 : sp48 + 2;
+        arg0->posX += var_v0;
+    }
+}
+#else
+void func_801CA3CC(struct_801CA704* arg0, char arg1);
 #pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA3CC.s")
+#endif
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA4F4.s")
+void func_801CA4F4(struct_801CA704* arg0, char c) {
+    if (c >= 0x20 && c <= 0xFF) {
+        func_801CA3CC(arg0, c);
+        return;
+    } else {
+        switch (c) {
+        case '\n':
+            arg0->posY += 32;
+            /* fallthrough */
+        case '\r':
+            arg0->posX = arg0->baseX;
+            break;
+        case '\t':
+            do {
+                func_801CA3CC(arg0, ' ');
+            } while ((arg0->posX - arg0->baseX) % 256);
+            break;
+        case '\0':
+            break;
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA5BC.s")
+void func_801CA5BC(struct_801CA704* arg0, const char* str, s32 arg2, size_t count) {
+    const char* s = str;
+    s32 n = arg2 * count;
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA618.s")
+    while (n != 0) {
+        func_801CA4F4(arg0, *s++);
+        n--;
+    }
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA670.s")
+void func_801CA618(struct_801CA704* arg0, const char* str) {
+    while (*str != 0) {
+        func_801CA4F4(arg0, *str++);
+    }
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA6A0.s")
+void* func_801CA670(void* arg, const char* str, size_t count) {
+    func_801CA5BC(arg, str, 1, count);
+    return arg;
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA6D8.s")
+void func_801CA6A0(struct_801CA704* arg0) {
+    arg0->callback = &func_801CA670;
+    arg0->posX = 0;
+    arg0->posY = 0;
+    arg0->baseX = 0;
+    arg0->baseY = 0;
+    arg0->unk18 = 0;
+    arg0->unk1A = 0;
+    arg0->color.rgba = 0;
+    arg0->unk1C = 0;
+    arg0->unk4 = 0;
+}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA6E4.s")
+void func_801CA6D8(struct_801CA704* arg0) {}
 
-#pragma GLOBAL_ASM("oot/ne0/asm/functions/n64dd/n64dd_801CA0B0/func_801CA704.s")
+s32 func_801CA6E4(struct_801CA704* arg0, const char* fmt, va_list args) {
+    return PrintUtils_VPrintf(&arg0->callback, fmt, args);
+}
+
+s32 func_801CA704(struct_801CA704* arg0, const char* fmt, ...) {
+    s32 ret;
+    va_list args;
+
+    va_start(args, fmt);
+    ret = func_801CA6E4(arg0, fmt, args);
+    va_end(args);
+
+    return ret;
+}
