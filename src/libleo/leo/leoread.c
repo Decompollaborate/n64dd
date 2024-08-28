baf95b81c4ceba792957357b88d9b40e50461896
cadmic <cadmic24@gmail.com>
2024-08-28 08:03:40 -0700

    Better leoread.c match

--- src/libleo/leo/leoread.c
+++ src/libleo/leo/leoread.c
@@ -12,22 +12,13 @@ void leoRead(void) {
 
 extern OSMesgQueue LEOc2ctrl_que;
 
-// Different from standard libleo version:
-// void leoRead_common(unsigned int offset);
-// //{
-// //    u32 tg_lba;
-// //    u32 tg_blocks;
-// //    u32 message;
-// //    u16 retry_cnt;
-// //    Label: invalid_lba @ 172;
-// //    Label: read_complete @ 524;
-// //}
 void leoRead_common(unsigned int offset) {
     u32 tg_lba = LEOcur_command->data.readWrite.lba;
     u32 tg_blocks = LEOcur_command->data.readWrite.transferBlks;
     u32 message;
+    u16 retry_cnt;
 
-    if ((tg_lba | tg_blocks) >> 0x10 != 0) {
+    if ((tg_lba | tg_blocks) & 0xFFFF0000) {
         goto invalid_lba;
     }
 
@@ -40,12 +31,12 @@ void leoRead_common(unsigned int offset) {
     }
 
     if (tg_blocks == 0) {
-        if (tg_lba < 0x10DC) {
-            LEOcur_command->header.sense = LEO_SENSE_NO_ADDITIONAL_SENSE_INFOMATION;
-            LEOcur_command->header.status = LEO_STATUS_GOOD;
-            return;
+        if (tg_lba >= 0x10DC) {
+            goto invalid_lba;
         }
-        goto invalid_lba;
+        LEOcur_command->header.sense = LEO_SENSE_NO_ADDITIONAL_SENSE_INFOMATION;
+        LEOcur_command->header.status = LEO_STATUS_GOOD;
+        return;
     }
 
     LEOtgt_param.lba = tg_lba;
@@ -53,10 +44,13 @@ void leoRead_common(unsigned int offset) {
     osSendMesg(&LEOc2ctrl_que, NULL, OS_MESG_NOBLOCK);
     osStartThread(&LEOinterruptThread);
 
-    while (true) {
+    for (;;) {
         osRecvMesg(&LEOcontrol_que, (OSMesg)&message, OS_MESG_BLOCK);
 
         switch (message) {
+            case 0x90000:
+                goto read_complete;
+
             case 0x80000:
                 leoC2_Correction();
                 LEOrw_flags &= ~0x4000;
@@ -67,11 +61,10 @@ void leoRead_common(unsigned int offset) {
                 LEOcur_command->header.sense = message;
                 LEOcur_command->header.status = LEO_STATUS_CHECK_CONDITION;
                 return;
-
-            case 0x90000:
-                LEOcur_command->header.sense = LEO_SENSE_NO_ADDITIONAL_SENSE_INFOMATION;
-                LEOcur_command->header.status = LEO_STATUS_GOOD;
-                return;
         }
     }
+
+read_complete:
+    LEOcur_command->header.sense = LEO_SENSE_NO_ADDITIONAL_SENSE_INFOMATION;
+    LEOcur_command->header.status = LEO_STATUS_GOOD;
 }
